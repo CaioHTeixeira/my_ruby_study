1af7fe479c594a9617b11aefd0e23594f4c510a5
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-10-21 16:13:43 -0300

    refactor: binary search

--- ruby_study/algorithms/binary_search.rb
+++ ruby_study/algorithms/binary_search.rb
@@ -1,17 +1,17 @@
 class BinarySearch
 
     def binary_search(array, left, right, value)
+        
         if right >= left
             mid = left + (right - left).div(2);
-            p mid
-            mid if value === array[mid]
-                
+            
+            return mid if value === array[mid]
+            
             if value > array[mid]
-                p array[mid]
-                binary_search(array, mid+1, right, value)
+                return binary_search(array, mid+1, right, value)
             end
             
-            binary_search(array, left, mid-1, value)
+            return binary_search(array, left, mid-1, value)
         end
 
         return -1
@@ -21,7 +21,7 @@ end
 class Main
     arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
     array_length = arr.length
-    value = 110
+    value = 130
 
     binary_search = BinarySearch.new
     p binary_search.binary_search(arr, 0, array_length - 1, value)
