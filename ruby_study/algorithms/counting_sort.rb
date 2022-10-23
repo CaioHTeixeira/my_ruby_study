e9e80ef6c2655745f67f44340291ce2340b875ed
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-10-23 16:40:19 -0300

    feat: counting sort

--- ruby_study/algorithms/counting_sort.rb
+++ ruby_study/algorithms/counting_sort.rb
@@ -1,5 +1,6 @@
 class CountingSort
     def counting_sort arr
+        sorted_array = []
         count_array = Array.new(10, 0)
 
         for item in arr do
@@ -7,13 +8,22 @@ class CountingSort
         end
 
         for item in 0..count_array.length - 2 do
-            # array_index = count_array.find_index item
-            p item
-            
             count_array[item + 1] = 
                 count_array[item] + count_array[item + 1] 
         end
+
+        for item in (arr.length - 1).downto 0 #de 9 ate 0
+            # p item
+            sorted_array[count_array[arr[item]] - 1] = arr[item]
+            --count_array[arr[item]]
+        end
+
+        # for item in 1..9 do
+        #     count_array[item] += count_array[item-1]
+        # end
         p count_array #[0, 2, 4, 4, 5, 6, 6, 7, 7, 7]
+        
+        return sorted_array
     end
 end
 
