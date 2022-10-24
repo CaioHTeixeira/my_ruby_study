8b6b343d99a64ea8004c825f7d3f5210f250766f
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-10-24 06:59:18 -0300

    refactor: maximize array

--- ruby_study/algorithms/maximize_array.rb
+++ ruby_study/algorithms/maximize_array.rb
@@ -1,14 +1,8 @@
 class MaximizeArray
     def maximize_array arr, k
         for item in 1..k do
-            sum_of_negative_numbers = 0
-            index_minimum_item = -1
-            minimum_item = 2147483647
-
-            for item in arr do
-                minimum_item = arr.min
-                index_minimum_item = arr.find_index minimum_item
-            end
+            minimum_item = arr.min
+            index_minimum_item = arr.find_index minimum_item
             
             break if minimum_item === 0
 
