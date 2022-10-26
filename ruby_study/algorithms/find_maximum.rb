7d818b0a787c9f319052b66165ec4e04bf71181b
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-10-25 22:30:34 -0300

    feat: minimum product

---
+++ ruby_study/algorithms/find_maximum.rb
@@ -0,0 +1,14 @@
+class FindMaximum
+    def find_maximum arr
+    
+    end
+end
+
+class Main
+    arr = [3, 10]
+
+    find_maximum = FindMaximum.new
+    output = find_maximum.find_maximum arr
+
+    p "Output: #{output}."
+end
---
+++ ruby_study/algorithms/minimum_product.rb
@@ -0,0 +1,35 @@
+class MinimumProduct
+    def minimum_product arr
+        negative_itens = []
+        minimum_product = 1
+
+        for item in arr
+            if item < 0
+                negative_itens.push item
+            end
+        end
+
+        if negative_itens.length % 2 === 0
+            index_greater_negative_item = 
+                arr.find_index negative_itens.max
+
+            arr.delete_at index_greater_negative_item
+        end
+
+        for item in arr
+            next if item === 0
+            minimum_product *= item
+        end
+
+        return minimum_product
+    end
+end
+
+class Main
+    arr = [-1, -1, -2, 4, 3]
+
+    minimum_product = MinimumProduct.new
+    output = minimum_product.minimum_product arr
+
+    p "Output: #{output}."
+end
