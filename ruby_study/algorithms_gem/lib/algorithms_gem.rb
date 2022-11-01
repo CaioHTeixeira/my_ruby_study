3b583e55f6631728edad75fd8c2b701d89787842
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-10-31 21:43:52 -0300

    chore: algorithms_gem

---
+++ ruby_study/algorithms_gem/lib/algorithms_gem.rb
@@ -0,0 +1,6 @@
+require "algorithms_gem/version"
+
+module AlgorithmsGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ ruby_study/algorithms_gem/lib/algorithms_gem/version.rb
@@ -0,0 +1,3 @@
+module AlgorithmsGem
+  VERSION = "0.1.0"
+end
