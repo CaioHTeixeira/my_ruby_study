4726f7dd3be9df5c960b2946a0b92b1118a0339d
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-03 04:47:20 -0300

    feat: fibonacci search test

--- ruby_study/algorithms_gem_level2/spec/lib/search/fibonacci_search_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/search/fibonacci_search_spec.rb
@@ -2,10 +2,20 @@ require './lib/search/fibonacci_search'
 
 RSpec.describe Asearch::FibonacciSearch do
     it "item found" do
-        
+        arr = [2, 3, 4, 10, 40]
+        value = 10
+        fibonacci_search = Asearch::FibonacciSearch.new
+            .fibonacci_search arr, value
+
+        expect(fibonacci_search).to eql 3
     end
 
     it "item not found" do
-        
+        arr = [2, 3, 4, 10, 40]
+        value = 11
+        fibonacci_search = Asearch::FibonacciSearch.new
+            .fibonacci_search arr, value
+
+        expect(fibonacci_search).to eql -1
     end
 end
