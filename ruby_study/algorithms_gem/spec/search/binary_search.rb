e34085b2cd51d0119ded37dbd93cf0cef35ea4b8
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-06 01:55:42 -0300

    feat: binary search test

--- ruby_study/algorithms_gem/spec/search/binary_search.rb
+++ ruby_study/algorithms_gem/spec/search/binary_search.rb
@@ -1,28 +1,23 @@
-class BinarySearch
+require './lib/search/binary_search'
 
-    def binary_search(array, left, right, value)
-        
-        if right >= left
-            mid = left + (right - left).div(2);
-            
-            return mid if value === array[mid]
-            
-            if value > array[mid]
-                return binary_search(array, mid+1, right, value)
-            end
-            
-            return binary_search(array, left, mid-1, value)
-        end
-
-        return -1
-    end
-end
-
-class Main
+RSpec.describe Asearch::BinarySearch do
     arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
     array_length = arr.length
-    value = 110
+    
+    it "item found" do
+        value = 110
+    
+        binary_search = Asearch::BinarySearch.new
+            .binary_search arr, 0, array_length - 1, value
+        
+        expect(binary_search).to eql 6
+    end
 
-    binary_search = BinarySearch.new
-    p binary_search.binary_search(arr, 0, array_length - 1, value)
+    it "item not found" do
+        value = 114
+        binary_search = Asearch::BinarySearch.new
+            .binary_search arr, 0, array_length - 1, value
+        
+        expect(binary_search).to eql -1
+    end
 end
