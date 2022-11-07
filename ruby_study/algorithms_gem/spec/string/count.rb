f9455b3f9da7a79c7fe2722ffb47059c452a88f5
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-06 23:02:53 -0300

    feat: missing characters tests

--- ruby_study/algorithms_gem/spec/string/count.rb
+++ ruby_study/algorithms_gem/spec/string/count.rb
@@ -1,31 +1,22 @@
 require './lib/string/count'
 
 RSpec.describe Pstring::Count do
+    input = "#GeeKs01fOr@gEEks07"
+    count = Pstring::Count.new.count_character input
+    
     it "Lowercase count is correct" do
-        input = "#GeeKs01fOr@gEEks07"
-        count = Pstring::Count.new.count_character input
-
         expect(count[:lowercase_amount]).to eql 8
     end
 
     it "Uppercase count is correct" do
-        input = "#GeeKs01fOr@gEEks07"
-        count = Pstring::Count.new.count_character input
-
         expect(count[:uppercase_amount]).to eql 5
     end
 
     it "Special character count is correct" do
-        input = "#GeeKs01fOr@gEEks07"
-        count = Pstring::Count.new.count_character input
-
         expect(count[:special_characters_amount]).to eql 2
     end
 
     it "Number count is correct" do
-        input = "#GeeKs01fOr@gEEks07"
-        count = Pstring::Count.new.count_character input
-
         expect(count[:numeric_values_amount]).to eql 4
     end
 end
--- ruby_study/algorithms_gem/spec/string/missing_character.rb
+++ ruby_study/algorithms_gem/spec/string/missing_character.rb
@@ -1,7 +1,19 @@
 require './lib/string/missing_character'
 
 RSpec.describe Pstring::MissingCharacter do
-    it "" do
-    
+    it "missing characters found" do
+        text = "The quick brown fox jumps"
+        missing_character = Pstring::MissingCharacter.new
+            .missing_character text
+
+        expect(missing_character).to eql 'adglvyz'
+    end
+
+    it "missing characters found" do
+        text = "welcome to geeksforgeeks"
+        missing_character = Pstring::MissingCharacter.new
+            .missing_character text
+
+        expect(missing_character).to eql 'abdhijnpquvxyz'
     end
 end
