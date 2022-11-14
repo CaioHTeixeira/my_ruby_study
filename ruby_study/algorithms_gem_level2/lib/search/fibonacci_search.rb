ab02d56dc89da982026a95d8e466b0fd01310749
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-14 15:50:04 -0300

    feat: fibonacci search finished

--- ruby_study/algorithms_gem_level2/lib/search/fibonacci_search.rb
+++ ruby_study/algorithms_gem_level2/lib/search/fibonacci_search.rb
@@ -1,8 +1,41 @@
 #A=Algorithm
 module Asearch
     class FibonacciSearch
-        def fibonacci_search arr, value
-            
+        def fibonacci_search arr, value, arr_length
+            offset = -1
+            fib_M2 = 0
+            fib_M1 = 1
+            fib_M = fib_M1 + fib_M2
+
+            while fib_M < arr_length do
+                fib_M2 = fib_M1
+                fib_M1 = fib_M
+                fib_M = fib_M2 + fib_M1
+            end
+
+            while fib_M > 1 do
+                i = offset + fib_M2 < arr_length - 1 ? offset + fib_M2
+                    : arr_length - 1
+                    
+                if arr[i] < value
+                    fib_M = fib_M1
+                    fib_M1 = fib_M2
+                    fib_M2 = fib_M - fib_M1
+                    offset = i
+                elsif arr[i] > value
+                    fib_M = fib_M2
+                    fib_M1 -= fib_M2
+                    fib_M2 = fib_M - fib_M1
+                else
+                    return i
+                end
+            end
+
+            if fib_M1 == 1 && arr[arr_length-1] == value
+                arr_length - 1
+            end
+
+            -1
         end
     end
 end
--- ruby_study/algorithms_gem_level2/spec/lib/search/fibonacci_search_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/search/fibonacci_search_spec.rb
@@ -3,18 +3,20 @@ require './lib/search/fibonacci_search'
 RSpec.describe Asearch::FibonacciSearch do
     it "item found" do
         arr = [2, 3, 4, 10, 40]
+        arr_length = arr.length
         value = 10
         fibonacci_search = Asearch::FibonacciSearch.new
-            .fibonacci_search arr, value
+            .fibonacci_search arr, value, arr_length
 
         expect(fibonacci_search).to eql 3
     end
 
     it "item not found" do
         arr = [2, 3, 4, 10, 40]
+        arr_length = arr.length
         value = 11
         fibonacci_search = Asearch::FibonacciSearch.new
-            .fibonacci_search arr, value
+            .fibonacci_search arr, value, arr_length
 
         expect(fibonacci_search).to eql -1
     end
