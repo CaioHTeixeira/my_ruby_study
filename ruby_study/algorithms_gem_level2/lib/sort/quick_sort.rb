6f719ba7739a74eb96ae063716ec1725b8620042
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-16 23:54:25 -0300

    feat: quick sort

--- ruby_study/algorithms_gem_level2/lib/sort/quick_sort.rb
+++ ruby_study/algorithms_gem_level2/lib/sort/quick_sort.rb
@@ -1,8 +1,36 @@
 #A=Algorithm
 module Asort
     class QuickSort
-        def quick_sort arr
+        def quick_sort arr, low, high
+            if low < high
+                partition_index = partition arr, low, high
+                quick_sort arr, low, partition_index - 1
+                quick_sort arr, partition_index + 1, high
+            end
+
             arr
+        end
+
+        def partition arr, low, high
+            pivot = arr[high]
+            i = low - 1
+
+            for j in low..high-1 do
+                if arr[j] < pivot
+                    i += 1
+                    swap_elements arr, i, j
+                end
+            end
+
+            swap_elements arr, i + 1, high
+
+            i + 1
+        end
+
+        def swap_elements arr, i, j
+            aux = arr[i]
+            arr[i] = arr[j]
+            arr[j] = aux
         end
     end
 end
--- ruby_study/algorithms_gem_level2/spec/lib/sort/quick_sort_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/sort/quick_sort_spec.rb
@@ -3,7 +3,7 @@ require './lib/sort/quick_sort'
 RSpec.describe Asort::QuickSort do
     it "Array Sorted" do
         arr = [10, 80, 30, 90, 40, 50, 70]
-        quick_sort = Asort::QuickSort.new.quick_sort arr
+        quick_sort = Asort::QuickSort.new.quick_sort arr, 0, arr.length - 1
 
         expect(quick_sort).to eql [10, 30, 40, 50, 70, 80, 90]
     end
