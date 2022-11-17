5fbac49b4fbbaab6974305d83c1208a6ec7f6d4c
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-17 13:10:33 -0300

    feat: minimum sum test

---
+++ ruby_study/algorithms_gem_level2/lib/greedy/minimum_sum.rb
@@ -0,0 +1,8 @@
+#A=Algorithm
+module Agreedy
+    class MinimumSum
+        def minimum_sum
+            
+        end
+    end
+end
---
+++ ruby_study/algorithms_gem_level2/spec/lib/greedy/minimum_sum_spec.rb
@@ -0,0 +1,23 @@
+describe './lib/greedy/minimum_sum'
+
+RSpec.describe Agreedy::MinimumSum do
+    it "Minimum sum with negative array numbers is correct" do
+        a = [1, 2, -3]
+        b = [-2, 3, -5]
+        k = 5
+        
+        minimum_sum = Agreedy::MinimumSum.new.minimum_sum a, b, k
+
+        expect(minimum_sum).to eql -31
+    end
+
+    it "Minimum sum is correct" do
+        a = [2, 3, 4, 5, 4]
+        b = [3, 4, 2, 3, 2]
+        k = 3
+        
+        minimum_sum = Agreedy::MinimumSum.new.minimum_sum a, b, k
+
+        expect(minimum_sum).to eql 25
+    end
+end
