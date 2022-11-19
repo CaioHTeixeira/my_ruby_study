cac6f20726a952906f105f79f52d24833b18c5cf
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-19 20:59:27 -0300

    chore: job sequencing

--- ruby_study/algorithms_gem_level2/lib/greedy/job_sequencing.rb
+++ ruby_study/algorithms_gem_level2/lib/greedy/job_sequencing.rb
@@ -1,8 +1,18 @@
 #A=Algorithm
 module Agreedy
     class JobSequencing
-        def job_sequencing input
-            
+        attr_accessor :job, :deadline, :profit
+        @@arr = []
+
+        def initialize job, deadline, profit
+            @job = job
+            @deadline = deadline
+            @profit = profit
+        end
+
+        def job_sequencing
+            @@arr << self
+            @@arr
         end
     end
 end
--- ruby_study/algorithms_gem_level2/spec/lib/greedy/job_sequencing_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/greedy/job_sequencing_spec.rb
@@ -2,20 +2,28 @@ require './lib/greedy/job_sequencing'
 
 RSpec.describe Agreedy::JobSequencing do
     it "Four Jobs passed" do
-        input = "Four Jobs with following deadlines and profits"
-        job_sequencing = Agreedy::JobSequencing.new
-            .job_sequencing input
+        job1 = Agreedy::JobSequencing.new('a', 4, 20).job_sequencing
+        job2 = Agreedy::JobSequencing.new('b', 1, 10).job_sequencing
+        job3 = Agreedy::JobSequencing.new('c', 1, 40).job_sequencing
+        job4 = Agreedy::JobSequencing.new('d', 1, 30).job_sequencing 
+        p job1[0][:job]
+        # job_sequencing = Agreedy::JobSequencing.new(arr).job_sequencing
 
-        expect(job_sequencing)
-            .to eql "Following is maximum profit sequence of jobs: c, a"
+        expect(arr)
+            .to eql "Following is maximum profit sequence of jobs: c, a, e"
     end
 
-    it "Five Jobs passed" do
-        input = "Five Jobs with following deadlines and profits"
-        job_sequencing = Agreedy::JobSequencing.new
-            .job_sequencing input
+    it "Five Jobs passed" do 
+        arr = [Agreedy::JobSequencing.new('a', 2, 100),
+            Agreedy::JobSequencing.new('b', 1, 19),
+            Agreedy::JobSequencing.new('c', 2, 27),
+            Agreedy::JobSequencing.new('d', 1, 25),
+            Agreedy::JobSequencing.new('e', 3, 15) 
+        ]
         
-        expect(job_sequencing)
-            .to eql "Following is maximum profit sequence of jobs: c, a, e"
+        # job_sequencing = Agreedy::JobSequencing.new(arr).job_sequencing
+
+        expect(arr)
+            .to eql "Following is maximum profit sequence of jobs: c, a"
     end
 end
