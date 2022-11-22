8e42ff323482f0935dae0d5f2543030d9c968b57
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-22 09:48:49 -0300

    feat: fractional knapsack finished

--- ruby_study/algorithms_gem_level2/lib/greedy/fractional_knapsack.rb
+++ ruby_study/algorithms_gem_level2/lib/greedy/fractional_knapsack.rb
@@ -7,7 +7,18 @@ module Agreedy
 
             arr.sort_by! { |item| item[:value] / item[:weight] }.reverse!
 
-            
+            arr.each_with_index do |item, index|
+                if sum_weight + arr[index][:weight] <= w
+                    sum_weight += item[:weight]
+                    output += item[:value] 
+                elsif sum_weight + arr[index][:weight] > w
+                    missing_value = w - sum_weight
+                    fraction = missing_value.fdiv arr[index][:weight]
+                    output += fraction * arr[index][:value]
+                end 
+            end
+
+            output.round 3
         end
     end
 end
