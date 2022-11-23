47d29f87e7db69247cbd5f62fafdaef61d13799d
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-11-23 19:19:44 -0300

    feat: paper cut add constructor and attrs

--- ruby_study/algorithms_gem_level2/lib/greedy/paper_cut.rb
+++ ruby_study/algorithms_gem_level2/lib/greedy/paper_cut.rb
@@ -1,6 +1,17 @@
 #A=Algorithm
 module Agreedy
     class PaperCut
-        
+        attr_accessor :height, :width
+
+        def initialize height, width
+            @height = height
+            @width = width
+        end
+
+        def paper_cut papercut 
+            output = 0
+            
+            output
+        end
     end
 end
