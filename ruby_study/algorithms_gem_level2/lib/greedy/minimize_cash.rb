6d7fbacb1056768373380284e4a60f63fd535463
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-12-12 04:56:34 -0300

    test: building initial test

---
+++ ruby_study/algorithms_gem_level2/lib/greedy/minimize_cash.rb
@@ -0,0 +1,8 @@
+#A=Algorithm
+module Agreedy
+    class MinimizeCash
+        def initialize
+            
+        end
+    end
+end
---
+++ ruby_study/algorithms_gem_level2/spec/lib/greedy/minimize_cash_spec.rb
@@ -0,0 +1,7 @@
+require './lib/greedy/minimize_cash'
+
+RSpec.describe Agreedy::MinimizeCash do
+    it "minimized cash flow" do
+        
+    end
+end
