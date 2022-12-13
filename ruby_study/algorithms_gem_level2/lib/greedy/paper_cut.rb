75a0edbc3352f5e8c4f2e7035b997bd00a3e6cb1
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-12-13 17:53:54 -0300

    fix: bug fixed

--- ruby_study/algorithms_gem_level2/lib/greedy/paper_cut.rb
+++ ruby_study/algorithms_gem_level2/lib/greedy/paper_cut.rb
@@ -1,17 +1,37 @@
 #A=Algorithm
 module Agreedy
     class PaperCut
-        attr_accessor :height, :width
+        attr_accessor :output
 
-        def initialize height, width
-            @height = height
-            @width = width
+        def initialize 
+            @output = 0
         end
 
-        def paper_cut papercut 
-            output = 0
+        def paper_cut width, height 
+            max_side_square = 0
+            quantity = 0
+
+            max_side_paper = height > width ? 
+                height : width
+
+            max_side_square = height < width ? 
+                height : width
             
-            output
+            quantity = max_side_paper / max_side_square if max_side_square != 0
+            
+            @output += quantity
+
+            if max_side_square != 0
+                if width == max_side_square 
+                    paper_cut(width, height - quantity*max_side_square)
+                end
+                
+                if height == max_side_square 
+                    paper_cut(width - quantity*max_side_square, height)
+                end
+            end
+
+            @output 
         end
     end
 end
--- ruby_study/algorithms_gem_level2/spec/lib/greedy/paper_cut_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/greedy/paper_cut_spec.rb
@@ -2,21 +2,21 @@ require './lib/greedy/paper_cut'
 
 RSpec.describe Agreedy::PaperCut do 
     it "Input 13 x 29 test" do
-        height = 29
         width = 13
+        height = 29
 
-        paper_cut = Agreedy::PaperCut.new height, width
-        output = Agreedy::PaperCut.new.paper_cut paper_cut
+        paper_cut = Agreedy::PaperCut.new #width, height
+        output = paper_cut.paper_cut width, height
         
         expect(output).to eql 9
     end
 
     it "Input 4 x 5 test" do
-        height = 4
-        weight = 5
+        width = 4
+        height = 5
 
-        paper_cut = Agreedy::PaperCut.new height, width
-        output = Agreedy::PaperCut.new.paper_cut paper_cut
+        paper_cut = Agreedy::PaperCut.new #width, height
+        output = paper_cut.paper_cut width, height
 
         expect(output).to eql 5
     end
