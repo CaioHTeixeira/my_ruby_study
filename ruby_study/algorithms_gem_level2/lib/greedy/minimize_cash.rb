e480775ab13afc1359c11ea0e97a25c4e270bad1
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-12-19 00:23:13 -0300

    feat(minimize_cash): add account class

--- ruby_study/algorithms_gem_level2/lib/greedy/minimize_cash.rb
+++ ruby_study/algorithms_gem_level2/lib/greedy/minimize_cash.rb
@@ -1,7 +1,40 @@
 #A=Algorithm
 module Agreedy
+    class Account
+        attr_accessor :name, :balance
+
+        def initialize name
+            @name = name
+            @balance = 0.0
+        end
+
+        def transfer other_account, value
+            if @balance >= value
+                debit(value)
+                deposit(other_account, value)
+            else
+                raise "Saldo insuficiente"
+            end
+        end
+        
+        def deposit other_account, value
+            other_account.balance += value
+        end
+
+        def debit value
+            @balance -= value
+        end
+    end
+
     class MinimizeCash
         def initialize
+            
+        end
+
+        def create_net_amount
+            
+        end
+        def minimize_cash
             
         end
     end
--- ruby_study/algorithms_gem_level2/spec/lib/greedy/minimize_cash_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/greedy/minimize_cash_spec.rb
@@ -2,6 +2,19 @@ require './lib/greedy/minimize_cash'
 
 RSpec.describe Agreedy::MinimizeCash do
     it "minimized cash flow" do
-        
+        p1 = Agreedy::Account.new "conta_pessoa1"
+        p2 = Agreedy::Account.new "conta_pessoa2"
+        p3 = Agreedy::Account.new "conta_pessoa3"
+
+        p1.deposit p1, 10000.0
+        p2.deposit p2, 10000.0
+        p3.deposit p3, 10000.0
+        p p1, p2, p3
+
+        net_definition = {
+            p1: 
+        }
+        minimize_cash = Agreedy::MinimizeCash.new
+        minimize_cash.create_net_amount 
     end
 end
