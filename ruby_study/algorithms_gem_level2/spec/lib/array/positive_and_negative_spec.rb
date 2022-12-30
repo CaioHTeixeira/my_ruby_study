393c3dd57e14f826e13223454cfaa875eac88371
Caio Teixeira <caioteixeiraaa@gmail.com>
2022-12-30 20:03:34 -0300

    test: adding 2 tests of positive and negative numbers

--- ruby_study/algorithms_gem_level2/spec/lib/array/positive_and_negative_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/array/positive_and_negative_spec.rb
@@ -3,17 +3,21 @@ require './lib/array/positive_and_negative'
 RSpec.describe PArray::PositiveAndNegative do
     it "first test" do
         input = [1, -3, 5, 6, -3, 6, 7, -4, 9, 10]
+        expected_output = [1, -3, 5, -3, 6, 6, 7, -4, 9, 10]
+
         output = PArray::PositiveAndNegative.new
             .positive_and_negative input
-
-        expect(output).to eql [1, -3, 5, -3, 6, 6, 7, -4, 9, 10]
+        
+        expect(output).to eql expected_output
     end
 
     it "second test" do
         input = [-1, 3, -5, 6, 3, 6, -7, -4, -9, 10]
+        expected_output = [3, -1, 6, -5, 3, -7, 6, -4, 10, -9]
+        
         output = PArray::PositiveAndNegative.new
             .positive_and_negative input
 
-        expect(output).to eql [3, -1, 6, -5, 3, -7, 6, -4, 10, -9]
+        expect(output).to eql expected_output
     end
 end
