75e1f9ed8368e5f1bfff38c937fa1a0b5945adb2
Caio Teixeira <caioteixeiraaa@gmail.com>
2023-01-02 19:27:20 -0300

    test: adding 2 tests

---
+++ ruby_study/algorithms_gem_level2/lib/date/sum_minute.rb
@@ -0,0 +1,8 @@
+#P=Problem
+module PDate
+    class SumMinute
+        def sum_minute time, k
+            
+        end
+    end
+end
---
+++ ruby_study/algorithms_gem_level2/spec/lib/date/sum_minute_spec.rb
@@ -0,0 +1,21 @@
+require './lib/date/sum_minute'
+
+RSpec.describe PDate::SumMinute do
+    it "k equal 21 test" do
+        T = "12:43" 
+        K = 21
+
+        output = PDate::SumMinute.new.sum_minute T, K
+
+        expect(output).to eql "13:04"
+    end
+
+    it "k equal 534 test" do
+        T = "20:39" 
+        K = 534
+
+        output = PDate::SumMinute.new.sum_minute T, K
+
+        expect(output).to eql "05:33"
+    end
+end
