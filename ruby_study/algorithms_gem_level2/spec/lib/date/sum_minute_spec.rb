da2cae7520c156683c774149d22f625b891fd881
Caio Teixeira <caioteixeiraaa@gmail.com>
2023-01-03 09:05:00 -0300

    refactor: refactoring tests

--- ruby_study/algorithms_gem_level2/spec/lib/date/sum_minute_spec.rb
+++ ruby_study/algorithms_gem_level2/spec/lib/date/sum_minute_spec.rb
@@ -2,19 +2,19 @@ require './lib/date/sum_minute'
 
 RSpec.describe PDate::SumMinute do
     it "k equal 21 test" do
-        T = "12:43" 
-        K = 21
+        t = "12:43" 
+        k = 21
 
-        output = PDate::SumMinute.new.sum_minute T, K
+        output = PDate::SumMinute.new.sum_minute t, k
 
         expect(output).to eql "13:04"
     end
 
     it "k equal 534 test" do
-        T = "20:39" 
-        K = 534
+        t = "20:39" 
+        k = 534
 
-        output = PDate::SumMinute.new.sum_minute T, K
+        output = PDate::SumMinute.new.sum_minute t, k
 
         expect(output).to eql "05:33"
     end
