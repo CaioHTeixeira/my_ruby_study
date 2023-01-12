39f5615878563b9851d6a8295f587d36cf368151
Caio Teixeira <caioteixeiraaa@gmail.com>
2023-01-11 23:06:20 -0300

    refactor: refactoring bubble sort

--- ruby_study/algorithms_gem_level1/lib/sort/bubble_sort.rb
+++ ruby_study/algorithms_gem_level1/lib/sort/bubble_sort.rb
@@ -4,22 +4,19 @@ module Asort
             
         def bubble_sort arr
 
-            for item in arr do
-                for item2 in arr do
-                    i = arr.find_index(item2)
-                    j = arr.find_index(item2) + 1
-
-                    unless j === arr.length
-                        if arr[i] > arr[j]
-                            aux = arr[i]
-                            arr[i] = arr[j]
-                            arr[j] = aux
+            for i in 0..arr.length - 1 do
+                for j in 0..arr.length - 1 do
+                    unless i === arr.length
+                        if arr[j] > arr[i]
+                            aux = arr[j]
+                            arr[j] = arr[i]
+                            arr[i] = aux
                         end
                     end
                 end
             end
 
-            return arr
+            arr
         end
     end
 end
