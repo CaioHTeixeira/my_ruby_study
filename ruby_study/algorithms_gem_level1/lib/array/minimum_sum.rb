e0f1f206fac31e02db1d7ae21a29fffd94ee47cc
Caio Teixeira <caioteixeiraaa@gmail.com>
2023-01-17 11:52:32 -0300

    refactor: refactoring minimum sum code

--- ruby_study/algorithms_gem_level1/lib/array/minimum_sum.rb
+++ ruby_study/algorithms_gem_level1/lib/array/minimum_sum.rb
@@ -5,7 +5,7 @@ module Aarray
         def minimum_sum arr
             min_value = arr.min
 
-            return min_value * (arr.length - 1)
+            min_value * (arr.length - 1)
         end
     end
 end
